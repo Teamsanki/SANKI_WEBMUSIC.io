c1039367d78f037db3ddec6176f702fd628467d9
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-27 16:11:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Check if user data is already saved in localStorage
+// Check if user data is already saved in localStorag
 window.onload = function () {
   const savedUsername = localStorage.getItem("username");
   const savedTelegram = localStorage.getItem("telegram");
