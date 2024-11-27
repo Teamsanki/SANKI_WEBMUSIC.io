fd4aa71678e6bde1eb5cbfbeed974722fa3af665
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-27 16:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let roomId = localStorage.getItem('roomId');
 
 // Static Playlist with Spotify Preview URLs
 const playlistData = [
-  { name: "Song 1 - Artist 1", preview_url: "https://p.scdn.co/mp3-preview/your_preview_url_1" },
+  { name: "Ishq di bajiyaan", preview_url: "https://firebasestorage.googleapis.com/v0/b/social-bite-skofficial.appspot.com/o/Sanki%2FIshq%20Di%20Baajiyaan%20-%20Diljit%20Dosanjh.mp3?alt=media&token=4e8f492c-57c1-44e9-8410-a6c4a0aa4109" },
   { name: "Song 2 - Artist 2", preview_url: "https://p.scdn.co/mp3-preview/your_preview_url_2" },
   { name: "Song 3 - Artist 3", preview_url: "https://p.scdn.co/mp3-preview/your_preview_url_3" },
 ];
@@ -81,8 +81,8 @@ function playSong(previewUrl) {
   audioPlayer.play();
 
   // Send message to Telegram Bot (you need to set up bot and chat_id here)
-  const botToken = 'YOUR_BOT_TOKEN';
-  const chatId = 'YOUR_CHAT_ID';
+  const botToken = '7902514308:AAGRWf0i1sN0hxgvVh75AlHNvcVpJ4j07HY';
+  const chatId = '-1002148651992';
   const message = `${username} played a song in room ${roomname} (Room ID: ${roomId})`;
 
   const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
